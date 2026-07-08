37630c9bed2d91653714c275f0d414a85b22b28c
Gab <ausejolamadridg@gmail.com>
2026-07-08 23:50:43 +0200

    Update TestConflict.cs

--- Dev_DungeonBunch/Assets/Scripts/TestConflict.cs
+++ Dev_DungeonBunch/Assets/Scripts/TestConflict.cs
@@ -5,7 +5,24 @@ public class TestConflict : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /* 
+        asdads
+        as
+        das
+        das
+        d
+        as
+        as
+        das
+        d
+        as
+        das
+        d
+        as
+        d
+        as
         
+         */
     }
 
     // Update is called once per frame
