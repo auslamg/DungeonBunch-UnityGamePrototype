d70a316766166ad84299cfc2be108ed517d34191
Gab <ausejolamadridg@gmail.com>
2026-07-21 21:17:41 +0200

    Jump + UI + Gizmos + GroundCheck
    - Added movement properties (getters) to CharacterController
    - Added dynamic damping and max velocity clamping to the CharacterController's rigidbody
    - Refactored interpolated input handling into raw input handling in CharacterController to avoid input delay
    - Added DebugUI and CharacterController debug UI references and code
    - Added GizmosUtil and DrawWireCapsule utility method
    - Added proper Gizmos to the CharacterController and GroundCheck
    - Implemented GroundCheck
    - Fixed Jump behaviour to obtain semi-predictable jump height. Added jump cooldown and jump dumping for this
    - Added CooldownTimer struct
    
    Debug UI is meant to be used as an alternative for console debugging, specially for physics and high performance cost operations. Further commits should make it toggleable to increase performance.
    
    DrawWireCapsule is meant to be used for other capsule colliders whose visualization would aid debugging. It should be complementary to base Unity gizmo methods.

--- Dev_DungeonBunch/Assets/Scripts/Player/BasicCharacterController.cs
+++ Dev_DungeonBunch/Assets/Scripts/Player/BasicCharacterController.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -5,17 +7,36 @@ using UnityEngine;
 public class BasicCharacterController : MonoBehaviour
 {
     [Header("Movement")]
+    [SerializeField] private float maxVelocity = 8f;
     [SerializeField] private float accelerationMultiplier = 1f;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpHeight = 2f;
     [SerializeField] private float jumpForceMultiplier = 1f;
+    [SerializeField] private CooldownTimer jumpCooldown;
+
 
     [Header("Ground Check Cache")]
     [SerializeField] private bool isGrounded = false;
-    [SerializeField] private RaycastHit groundCheckHit;
+    [SerializeField] private RaycastHit groundCheckHitInfo;
+
+    [Header("Damping Cache")]
+    private float baseLinearDamping;
+
+    [Header("Movement Getters")]
+    private float Velocity => rb.linearVelocity.magnitude;
+    private Vector3 FlatLinearVelocity => new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+    private float FlatVelocity => FlatLinearVelocity.magnitude;
 
     [Header("References")]
     [SerializeField] private Rigidbody rb;
     [SerializeField] private CapsuleCollider capsuleCollider;
     [SerializeField] private GroundCheck groundCheck;
+
+    [Header("Debug UI")]
+    [SerializeField] private TMP_Text t1;
+    [SerializeField] private TMP_Text t2;
+    [SerializeField] private TMP_Text t3;
 
     private Vector3 movementInput;
     private bool jumpPressed;
@@ -25,11 +46,19 @@ public class BasicCharacterController : MonoBehaviour
         TryGetComponent(out rb);
         TryGetComponent(out capsuleCollider);
         TryGetComponent(out groundCheck);
+
+        baseLinearDamping = rb.linearDamping;
     }
 
     void Update()
     {
-        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log($"Jump height = {maxY.ToString("f2")}");
+            maxY -= 0.1f;
+        }
+
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -37,27 +66,87 @@ public class BasicCharacterController : MonoBehaviour
         }
     }
 
+    private float maxY;
+    private bool jumpDamping = false;
+
     void FixedUpdate()
     {
+        //DEBUG
+        maxY = Mathf.Max(maxY, transform.position.y);
+
+        isGrounded = groundCheck.isGrounded(out groundCheckHitInfo);
+
+        if (movementInput.magnitude != 0f || !isGrounded || jumpDamping)
+        {
+            rb.linearDamping = 0;
+        }
+        else
+        {
+            rb.linearDamping = baseLinearDamping;
+        }
+
         rb.AddForce(
             movementInput * accelerationMultiplier * Time.fixedDeltaTime,
             ForceMode.VelocityChange
         );
 
+
+        // Clamp velocity
+        var clampedFlatVector = Vector3.ClampMagnitude(FlatLinearVelocity, maxVelocity);
+        rb.linearVelocity = new(clampedFlatVector.x, rb.linearVelocity.y, clampedFlatVector.z);
+
+        HandleJump();
+        UpdateUI();
+    }
+
+    private void HandleJump()
+    {
+        jumpCooldown.Tick(Time.deltaTime);
         if (jumpPressed)
         {
             jumpPressed = false;
 
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-            rb.AddForce(Vector3.up * jumpForceMultiplier, ForceMode.VelocityChange);
+            if (isGrounded && jumpCooldown.IsTicking())
+            {
+                // Jump
+                jumpDamping = true;
+                rb.linearDamping = 0;
+
+                float gravityTickCompensation = -(Physics.gravity.y * Time.deltaTime) / 2;
+
+                float jumpVelocity = Mathf.Sqrt(2f * Mathf.Abs(Physics.gravity.y) * jumpHeight) + gravityTickCompensation;
+
+                rb.linearVelocity = new Vector3(
+                    rb.linearVelocity.x,
+                    jumpVelocity,
+                    rb.linearVelocity.z
+                );
+
+                /* rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+                rb.AddForce(Vector3.up * jumpForceMultiplier, ForceMode.VelocityChange); */
+
+                jumpCooldown.Reset();
+            }
+        }
+        if (jumpCooldown.IsTicking())
+        {
+            jumpDamping = false;
         }
     }
 
-    
+    private void UpdateUI()
+    {
+        t1.text = $"FlatVelocity = {FlatVelocity.ToString("f2")}";
+        t2.text = $"Velocity = {Velocity.ToString("f2")}";
+        t3.text = $"Velocity = {rb.linearVelocity.magnitude.ToString("f2")}";
+    }
 
     void OnDrawGizmos()
     {
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireCube(transform.position, new(capsuleCollider.radius * 2, capsuleCollider.height, capsuleCollider.radius * 2));     
+        Gizmos.color = Color.limeGreen;
+        Gizmos.DrawWireCube(transform.position, new(capsuleCollider.radius * 2, capsuleCollider.height, capsuleCollider.radius * 2));
+
+        Gizmos.color = Color.white;
+        GizmosUtil.DrawCapsule(capsuleCollider);
     }
 }
--- Dev_DungeonBunch/Assets/Scripts/Player/GroundCheck.cs
+++ Dev_DungeonBunch/Assets/Scripts/Player/GroundCheck.cs
@@ -1,33 +1,68 @@
+using System;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
     [Header("Spherecast")]
-    [SerializeField] float radius = 1f;
+    [SerializeField] float radiusMultiplier = 0.8f;
     [SerializeField] float maxDistance = 1f;
-    [SerializeField] Transform origin;
     [SerializeField] LayerMask layerMask;
-    
-    private Ray DownRay => new(origin.position, Vector3.down);
-    
+    Vector3 Origin => gameObject.transform.position - new Vector3(0, capsuleCollider.bounds.extents.y / 2, 0);
+    Vector3 CastedOrigin => Origin + Vector3.down * ranDistance;
+    Vector3 ContactPoint => IsGrounded ? hitInfo.point : CastedOrigin;
+    float Radius => capsuleCollider.radius * radiusMultiplier;
+
+    [Header("Cache")]
+    [SerializeField] private bool IsGrounded;
+    [SerializeField] private RaycastHit hitInfo;
+    [SerializeField] private float ranDistance = 0;
+
+    [Header("References")]
+    [SerializeField] CapsuleCollider capsuleCollider;
+
+    private Ray DownRay => new(Origin, Vector3.down);
+
+    void OnValidate()
+    {
+        TryGetComponent(out capsuleCollider);
+    }
+
     public bool isGrounded()
     {
-        return Physics.SphereCast(DownRay, radius, maxDistance, layerMask);
+        IsGrounded = Physics.SphereCast(DownRay, Radius, out hitInfo, maxDistance, layerMask);
+        ranDistance = IsGrounded ? hitInfo.distance : maxDistance;
+
+        return IsGrounded;
     }
 
-    public bool isGrounded(out RaycastHit hit)
+    public bool isGrounded(out RaycastHit hitInfo)
     {
-        return Physics.SphereCast(DownRay, radius, out hit, maxDistance, layerMask);
-    }
+        IsGrounded = Physics.SphereCast(DownRay, Radius, out this.hitInfo, maxDistance, layerMask);
+        hitInfo = this.hitInfo;
+        ranDistance = IsGrounded ? hitInfo.distance : maxDistance;
 
-    private void LateUpdate() {
-        
+        return IsGrounded;
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(gameObject.transform.position, Vector3.one);
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(Origin, 0.02f);
+
+        Handles.DrawDottedLine(Origin, Origin + Vector3.down * ranDistance, 1);
+
+        Gizmos.color = IsGrounded ? Color.green : Color.red;
+        Handles.color = IsGrounded ? Color.green : Color.red;
+
+        Gizmos.DrawSphere(Origin + Vector3.down * ranDistance, 0.05f);
+        Gizmos.DrawWireSphere(Origin + Vector3.down * ranDistance, capsuleCollider.radius * radiusMultiplier);
+
+        if (IsGrounded)
+        {
+            Handles.DrawDottedLine(Origin + Vector3.down * ranDistance, ContactPoint, 1);
+            Gizmos.DrawSphere(ContactPoint, 0.05f);
+        }
     }
 }
